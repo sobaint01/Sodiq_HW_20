9d2b68a93d6c318e64dce691b6e04dcc52ad9af7
sobaint01 <walebellomail@gmail.com>
2022-02-17 16:59:56 -0500

    Sodia_HW_20
    Homework 20 is the project page and the case study page for my portfolio

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$("#project").hover( function(){
+   $(".navChildproject").slideToggle();
+});
+
+$("#about").hover( function(){
+  $(".navChildabout").slideToggle();
+});
+
+$("#resume").hover( function(){
+  $(".navChildresume").slideToggle();
+});
+
+$("#contact").hover( function(){
+  $(".navChildcontact").slideToggle();
+});
+
+$("#searchIcon").hover( function(){
+  $(".searchform").css("display","flex");
+});
+
+//burger toggle
+
+//$(".burgerIcon").on("click")( function(){
+ //$(".navChildburger","display","flex").slideToggle();
+//});
+
+//$("button").click( function(){
+ // $('.animate').animate( {left: '250px'}, 1000 );
+//});
+
